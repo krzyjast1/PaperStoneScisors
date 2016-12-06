5f775ff245d26e8efe1b1c60af77accf796d31a1
krzyjast1 <krzyjast1@gmail.com>
2016-12-06 23:58:36 +0100

    Engine done

--- PaperStoneScisors/Engine.cs
+++ PaperStoneScisors/Engine.cs
@@ -11,6 +11,8 @@ namespace PaperStoneScisors
         private static UserPlayer UserPlayer;
         private static CPUPlayer CpuPlayer;
 
+        public enum Win { USER, CPU, DRAW};
+
         public static void startEngine() //method which configure game event
         {
             Console.Write("Podaj nick: ");
@@ -20,7 +22,7 @@ namespace PaperStoneScisors
             UserPlayer = new UserPlayer(nick);
             CpuPlayer = new CPUPlayer();
 
-            for(int i = 0; i<10; i++)
+            for(int i = 0; i<3; i++)
             {
                 Console.WriteLine("Ładowanie ");
                 Thread.Sleep(200);
@@ -41,7 +43,7 @@ namespace PaperStoneScisors
 
         public static void play()
         {
-            while( CpuPlayer.pointCounter <5 || UserPlayer.pointCounter <5 )
+            while(true)
             {
                 Console.Clear();
                 showNecessaryInfo();
@@ -51,23 +53,100 @@ namespace PaperStoneScisors
                 key = Console.ReadKey();
                 Console.Clear();
                 CpuPlayer.choose();
-                
+
+                Win winner = new Win();
+
                 switch (key.Key)
                 {
-                    case ConsoleKey.K: UserPlayer.choice = Player.Choice.STONE; Console.WriteLine(UserPlayer.ToString());    break;
-                    case ConsoleKey.P: UserPlayer.choice = Player.Choice.PAPER; Console.WriteLine(UserPlayer.ToString()); break;
-                    case ConsoleKey.N: UserPlayer.choice = Player.Choice.SCISSORS; Console.WriteLine(UserPlayer.ToString()); break;
+                    case ConsoleKey.K: UserPlayer.choice = Player.Choice.STONE; winner = whoIsWinner(); break;
+                    case ConsoleKey.P: UserPlayer.choice = Player.Choice.PAPER; winner = whoIsWinner(); break;
+                    case ConsoleKey.N: UserPlayer.choice = Player.Choice.SCISSORS; winner = whoIsWinner(); break;
                     case ConsoleKey.Escape: return;
-                    default: Console.WriteLine("Zły klawisz. K - kamień. P - papier. N - nożyczki"); break;
+                    default: Console.WriteLine("Zły klawisz. K - kamień. P - papier. N - nożyczki"); Thread.Sleep(1000);  winner = Win.DRAW ; break;
                 }
-                Console.WriteLine(CpuPlayer.ToString());
-                Thread.Sleep(2000);
+
+                if (key.Key == ConsoleKey.K || key.Key == ConsoleKey.N || key.Key == ConsoleKey.P)
+                {
+                    Console.WriteLine(showChoice());
+                    Thread.Sleep(1000);
+                }
+
+                switch(winner)
+                {
+                    case Win.USER: UserPlayer.pointCounter++; Console.WriteLine(UserPlayer.nick + " wygrał."); break;
+                    case Win.CPU: CpuPlayer.pointCounter++; Console.WriteLine("Komputer wygrał."); break;
+                    case Win.DRAW: Console.WriteLine("Był remis."); break;
+                }
+
+                Thread.Sleep(1050);
+                Console.Clear();
+
+                if(UserPlayer.pointCounter == 5)
+                {
+                    showWinnerMessage(Win.USER);
+                    break;
+                }
+                else if(CpuPlayer.pointCounter == 5)
+                {
+                    showWinnerMessage(Win.CPU);
+                    break;
+                }
             }
         }
 
         public static void showNecessaryInfo()
         {
             Console.WriteLine(UserPlayer.nick + ": " + UserPlayer.pointCounter + "       Komputer: " + CpuPlayer.pointCounter);
+        }
+
+        public static Win whoIsWinner()
+        {
+            if (UserPlayer.choice == Player.Choice.STONE && CpuPlayer.choice == Player.Choice.STONE) return Win.DRAW;
+            else if (UserPlayer.choice == Player.Choice.PAPER && CpuPlayer.choice == Player.Choice.PAPER) return Win.DRAW;
+            else if (UserPlayer.choice == Player.Choice.SCISSORS && CpuPlayer.choice == Player.Choice.SCISSORS) return Win.DRAW;
+            else if (UserPlayer.choice == Player.Choice.PAPER && CpuPlayer.choice == Player.Choice.STONE) return Win.USER;
+            else if (UserPlayer.choice == Player.Choice.STONE && CpuPlayer.choice == Player.Choice.SCISSORS) return Win.USER;
+            else if (UserPlayer.choice == Player.Choice.SCISSORS && CpuPlayer.choice == Player.Choice.PAPER) return Win.USER;
+            else if (UserPlayer.choice == Player.Choice.SCISSORS && CpuPlayer.choice == Player.Choice.STONE) return Win.CPU;
+            else if (UserPlayer.choice == Player.Choice.STONE && CpuPlayer.choice == Player.Choice.PAPER) return Win.CPU;
+            else if (UserPlayer.choice == Player.Choice.PAPER && CpuPlayer.choice == Player.Choice.SCISSORS) return Win.CPU;
+            else throw new Exception("Błąd systemu sprawdzania wyników");
+        }
+        public static void showWinnerMessage(Win winner)
+        {
+            if(winner == Win.USER)
+            {
+                Console.WriteLine(UserPlayer.nick + " wygrał!!!.");
+                showNecessaryInfo();
+            }
+            else if(winner == Win.CPU)
+            {
+                Console.WriteLine("Komputer wygrał :(");
+                showNecessaryInfo();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Wciśnij dowolny klawisz aby kontynuować...");
+            Console.ReadKey();
+        }
+
+        public static string showChoice()
+        {
+            string msgCPU = "";
+            string msgUSER = "";
+            switch(UserPlayer.choice)
+            {
+                case Player.Choice.PAPER: msgUSER = "Papier"; break;
+                case Player.Choice.STONE: msgUSER = "Kamień"; break;
+                case Player.Choice.SCISSORS: msgUSER = "Nożyczki"; break;
+            }
+            switch (CpuPlayer.choice)
+            {
+                case Player.Choice.PAPER: msgCPU = "Papier"; break;
+                case Player.Choice.STONE: msgCPU = "Kamień"; break;
+                case Player.Choice.SCISSORS: msgCPU = "Nożyczki"; break;
+            }
+
+            return "Komputer: " + msgCPU + "\n" + UserPlayer.nick + ": " + msgUSER;
         }
     }
 }
